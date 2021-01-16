c2f8134c9bd49911844ff49252feff2b3ff36b9d
Atoleiyu <gaogao1@hotmail.co.jp>
2021-01-16 22:43:53 +0900

    詳細不明

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,7 @@
 <?php
 try {
-  $db = new  PDO('mysql:dbname=anacom_members;host=localhost;port=8889;charset=utf8', 'root', 'root');
+  // $db = new  PDO('mysql:dbname=anacom_members;host=localhost;port=8889;charset=utf8', 'root', 'root');
+  $db = new  PDO('mysql:dbname=anacom_members;host=localhost;port=118.27.116.243;charset=utf8', 'root', '@Kiara0517');
 } catch(PDOException $e) {
   echo 'DB接続エラー' . $e->getMessage();
 }
