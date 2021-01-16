df73f8e849d974257877add5f587a762dd1c4b47
稲葉慎太郎 <Shintaro@inabashintarou-no-MacBook-Pro.local>
2021-01-16 14:15:14 +0900

    DB関連の修正をしました

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,9 @@
 <?php
 try {
+  // mamp用↓
   $db = new  PDO('mysql:dbname=anacom_members;host=localhost;port=8889;charset=utf8', 'root', 'root');
+  // vps用↓
+  // $db = new  PDO('mysql:dbname=anacom_members;host=localhost;port=118.27.116.243;charset=utf8', 'root', '@Kiara0517');
 } catch(PDOException $e) {
   echo 'DB接続エラー' . $e->getMessage();
 }
